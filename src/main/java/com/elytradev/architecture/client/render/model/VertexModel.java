953888df66b0a21fb5be048afcf9cab0ad4e8547
darkevilmac <bk1325@gmail.com>
2017-09-17 17:31:36 -0700

    Update to 1.12.1

--- src/main/java/com/elytradev/architecture/client/render/model/VertexModel.java
+++ src/main/java/com/elytradev/architecture/client/render/model/VertexModel.java
@@ -24,7 +24,6 @@
 
 package com.elytradev.architecture.client.render.model;
 
-import com.google.common.base.Function;
 import net.minecraft.client.renderer.block.model.IBakedModel;
 import net.minecraft.client.renderer.texture.TextureAtlasSprite;
 import net.minecraft.client.renderer.vertex.VertexFormat;
@@ -35,6 +34,7 @@ import net.minecraftforge.common.model.TRSRTransformation;
 
 import java.util.Collection;
 import java.util.Collections;
+import java.util.function.Function;
 
 public class VertexModel implements IModel {
 
