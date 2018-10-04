ceb5cd41e02aa9cab074398cf7a6925e0e9dd1ea
Benjamin K <bk1325@gmail.com>
2018-10-04 14:00:55 -0700

    You have to import a class to use it.

--- src/main/java/com/elytradev/architecture/common/block/BlockShape.java
+++ src/main/java/com/elytradev/architecture/common/block/BlockShape.java
@@ -32,6 +32,7 @@ import net.minecraft.block.Block;
 import net.minecraft.block.material.Material;
 import net.minecraft.block.properties.IProperty;
 import net.minecraft.block.properties.PropertyInteger;
+import net.minecraft.block.state.BlockFaceShape;
 import net.minecraft.block.state.IBlockState;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.player.EntityPlayer;
