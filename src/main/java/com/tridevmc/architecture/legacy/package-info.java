94e2b3721d3198c54389c869b538e50686688911
darkevilmac <bk1325@gmail.com>
2023-05-11 23:13:02 -0700

    Remove sawbench textures as they pull from vanilla now, add package-info.java for legacy code to explain why it exists.

---
+++ src/main/java/com/tridevmc/architecture/legacy/package-info.java
@@ -0,0 +1,6 @@
+/**
+ * Contains legacy code from the original ArchitectureCraft mod by gcewing.
+ * <p>
+ * This code is intended to be removed before the next major release and is only present to allow for a smoother rewrite.
+ */
+package com.tridevmc.architecture.legacy;
